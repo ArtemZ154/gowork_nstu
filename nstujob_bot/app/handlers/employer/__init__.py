b0b50c6e26f21c2e7e4a7238d2544b2d7e6fae79
ArtemZ154 <Artemz154@ya.ru>
2025-04-08 22:27:23 +0700

    Файлы __init__ для всех роутеров

--- nstujob_bot/app/handlers/employer/__init__.py
+++ nstujob_bot/app/handlers/employer/__init__.py
@@ -1,1 +1,14 @@
-pass
+# app/handlers/admin/__init__.py
+from aiogram import Router
+
+# Импортируем роутеры из каждого модуля админских функций
+# Предполагаем, что в каждом .py файле есть переменная 'router = Router()'
+from . import applicant_selection, job_posting, view_jobs, registration
+
+# Создаем единый роутер для всех команд работодателя
+employer_router = Router(name="employer_features")
+
+employer_router.include_router(applicant_selection.router)
+employer_router.include_router(job_posting.router)
+employer_router.include_router(view_jobs.router)
+employer_router.include_router(registration.router)
